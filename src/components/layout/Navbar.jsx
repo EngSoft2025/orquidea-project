aa8309658522f023721ac2deb6cbf77c39e87015
DeguShi <128547705+DeguShi@users.noreply.github.com>
2025-05-16 11:51:18 -0300

    Ideia inicial de desing para paginaPesquisador, será aprimorada após funcionalidades de busca mergidas

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SearchBar from "../ui/SearchBar";
 
-const Navbar = () => (
-  <div className="navbar">
-    <Link to="/" style={{ display: "flex", alignItems: "center", textDecoration: "none" }}>
-      <img className="logo_img" src="../../../public/logo.png" alt="Logo" />
-      <span className="logo" style={{color: "blue"}}>Orquidea</span>
+const Navbar = ({ showSearchBar = false, extraClass = "" }) => (
+  <div className={`navbar ${extraClass}`}>
+    <Link
+      to="/"
+      style={{ display: "flex", alignItems: "center", textDecoration: "none" }}
+    >
+      <img className="logo_img" src="/logo.png" alt="Logo" />
+      <span className="texto_orquidea">Orquidea</span>
     </Link>
+
+    {showSearchBar && <SearchBar />}
   </div>
 );
 
