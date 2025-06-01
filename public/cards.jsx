e8cf2b0cdfe749956ba61fe948213dbe5d8fcd9f
MatheusPaivaa <matheuzu11@gmail.com>
2025-06-01 17:10:39 -0300

    ajustando branch

--- public/cards.jsx
+++ public/cards.jsx
@@ -1,25 +1,24 @@
 const cards = [
-    {
-      icon: "./medal.png",
-      title: "Showcase Achievements",
-      text: "Highlight your research accomplishments, awards, and milestones in one place"
+  {
+    icon: "./medal.png",
+    title: "Artigos em Alta",
+    text: "Encontre os Artigos Mais Relevantes da sua área!"
+  },
+  {
+    icon: "./graph.png",
+    title: "Artigos em Alta",
+    text: "Monitor your citations, h-index, and other key metrics with interactive visualizations."
+  },
+  {
+      icon: "./book.png",
+      title: "Artigos em Alta",
+      text: "Automatically import your publications from Google Scholar and ORCID with one click."
+  },
+  {
+      icon: "./people.png",
+      title: "Comparar",
+      text: "Compare pesquisadores com base em diferentes métricas!"
     },
-    {
-      icon: "./graph.png",
-      title: "Track Impact",
-      text: "Monitor your citations, h-index, and other key metrics with interactive visualizations."
-    },
-    {
-        icon: "./book.png",
-        title: "Import Publications",
-        text: "Automatically import your publications from Google Scholar and ORCID with one click."
-    },
-    {
-        icon: "./people.png",
-        title: "Grow Network",
-        text: "Connect with peers, discover potential collaborators, and expand your research network."
-      },
-  ];
-  
-  export default cards;
-  
+];
+ 
+export default cards;
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,23 +5,23 @@ import SearchBar from "../components/ui/SearchBar";
 import cards from "../../public/cards";
 import { Link } from "react-router-dom";
 import FlashCard from "../components/ui/FlashCard";
-
+ 
 const Index = () => (
-  <div className="pageWrapper">
+  <div>
     <section className="section_search">
       <div className="logo-pos">
         <img src="/logo.png" alt="Logo" className="logo_img" />
         <span className="texto_orquidea">Orquidea</span>
       </div>
-
+ 
       <SearchBar />
     </section>
-
+ 
     <section className="section_comp">
       {cards.map((card, i) => (
         <Link
           key={i}
-          to={`/${card.title.toLowerCase().replace(/\s+/g, '-')}`}  // A URL será construída com base no título
+          to={`/${card.title.toLowerCase().replace(/\s+/g, '-')}`}
           className="card-link"
         >
           <FlashCard
@@ -32,9 +32,9 @@ const Index = () => (
         </Link>
       ))}
     </section>
-
+ 
     <Footer />
   </div>
 );
-
+ 
 export default Index;
