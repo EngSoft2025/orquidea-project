cbe904c141fe5167dae98992ad3c69cc58388b64
MatheusPaivaa <matheuzu11@gmail.com>
2025-06-13 20:05:37 -0300

    Revert "artigos em alta"
    This reverts commit fd7225e2038031ddead89f989a28eaff1d354b79.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Index from "./pages/Index";
 import PaginaPesquisador from "./pages/PaginaPesquisador"; 
 import PaginaPesquisaNome from "./pages/PaginaPesquisaNome"; 
 import CompararPesquisador from "./pages/CompararPesquisador"; 
-import ArtigosEmAlta from "./pages/ArtigosEmAlta"; 
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <Route path="/pesquisaID" element={<PaginaPesquisador />} />
         <Route path="/pesquisa" element={<PaginaPesquisaNome />} />
         <Route path="/comparar" element={<CompararPesquisador />} />
-        <Route path="/artigos-em-alta" element={<ArtigosEmAlta />} />
       </Routes>
     </Router>
   );
