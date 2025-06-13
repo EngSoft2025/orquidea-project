fd7225e2038031ddead89f989a28eaff1d354b79
MatheusPaivaa <matheuzu11@gmail.com>
2025-06-13 20:00:49 -0300

    artigos em alta

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Index from "./pages/Index";
 import PaginaPesquisador from "./pages/PaginaPesquisador"; 
 import PaginaPesquisaNome from "./pages/PaginaPesquisaNome"; 
 import CompararPesquisador from "./pages/CompararPesquisador"; 
+import ArtigosEmAlta from "./pages/ArtigosEmAlta"; 
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/pesquisaID" element={<PaginaPesquisador />} />
         <Route path="/pesquisa" element={<PaginaPesquisaNome />} />
         <Route path="/comparar" element={<CompararPesquisador />} />
+        <Route path="/artigos-em-alta" element={<ArtigosEmAlta />} />
       </Routes>
     </Router>
   );
